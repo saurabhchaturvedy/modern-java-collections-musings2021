ce86124b030c4fc1a0600ccc9196ad569b8542f5
Saurabh Chaturvedi <saurabh.chaturvedi@mendix.com>
2021-06-01 22:14:00 +0530

    learning modern java collections

---
+++ src/main/java/Review_Generalization/Autobox.java
@@ -0,0 +1,21 @@
+package Review_Generalization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Autobox {
+
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        l.add(new Integer(5)); // Autoboxing
+        Integer iw = (Integer) l.get(0);
+        int i = iw.intValue();
+        System.out.println(i);
+        l.add(8);
+        Integer i2 = (Integer) l.get(1); // Auto-boxing
+        i=i2;  // Auto-unboxing
+        System.out.println(i);
+
+
+    }
+}
